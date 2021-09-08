4493382436e2c14226763b59397c48eeb86a313e
MADEWGN <90078192+MADEWGN@users.noreply.github.com>
2021-09-08 12:50:15 +0800

    Create config.py

---
+++ ffmpegbot/config.py
@@ -0,0 +1,9 @@
+from ffmpegbot.sample_config import Config
+
+class Development(Config):
+  APP_ID = "2345226"
+  API_HASH = "6cc6449dcef22f608af2cf7efb76c99d"
+  TG_BOT_TOKEN = "1955268284:AAF0ygNnHXOhl3Qlzwqtag5AWtRhIU6OjNM"
+  AUTH_USERS = [
+    1207375389
+  ]
